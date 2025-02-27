e26b29e9043939a18d4d71dbd2feaaa6ca7e5cd4
nguyendien9203 <ndien9203@gmail.com>
2025-02-27 10:19:37 +0700

    Add model

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
+const bodyParser = require("body-parser");
 //const connectDB = require('./config/db');
 require("dotenv").config();
 
@@ -9,7 +10,7 @@ require("dotenv").config();
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.use(cors());
 app.use(morgan("dev"));
 
