f130f730832e4807baf6085484cc8e7674252bc5
PhucNH812 <hphucxma2004@gmail.com>
2025-03-06 00:49:06 +0700

    feat(aws): set up connection to AWS S3, develop file API to work with S3 (upload, delete, check-exist)

---
+++ src/routes/s3.route.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const s3Router = express.Router();
+const {uploadSingleFile, deleteFileOnS3, checkFileExistsOnS3} = require("../controllers/s3.controller");
+// Need a middleware to decode token, will do it later and add to routes
+const { authenticateToken } = require("../middlewares/auth");
+const multer = require("multer");
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
+
+s3Router.post("/upload", upload.single("file"), uploadSingleFile);
+s3Router.delete("/delete/:fileName", deleteFileOnS3 );
+s3Router.get("/file-exist/:fileName", checkFileExistsOnS3);
+
+module.exports = s3Router;
