3c6f24f6ac4046b2d7d507ee8d767c390253053f
nguyendien9203 <ndien9203@gmail.com>
2025-03-09 10:05:47 +0700

    feat(user): hash password before saving user

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const Role = require("./role.model");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema(
   {
@@ -68,5 +69,17 @@ const userSchema = new mongoose.Schema(
   },
   { timestamps: true }
 );
+
+// hashed password
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
 
 module.exports = mongoose.model("User", userSchema);
