ae7c493a3223c45bc869e2ae6d4627ae05a84cca
PhucNH812 <hphucxma2004@gmail.com>
2025-03-09 19:35:03 +0700

    feat(backend-route): add API routes for candidate management

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,13 @@ const express = require("express");
 const authRoutes = require("./auth.route");
 const jobRoutes = require("./job.route");
 const s3Routes = require("./s3.route");
+const candidateRoutes = require("./candidate.route");
 
 const router = express.Router();
 
 router.use("/auth", authRoutes);
 router.use("/job", jobRoutes);
 router.use("/s3", s3Routes);
+router.use("/candidate", candidateRoutes);
 
 module.exports = router;
