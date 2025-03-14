2e10e9c9d8cfd2e5dbceee30e6bafd0cf119d06b
Anhmn03 <anhmnhe176487@fpt.edu.vn>
2025-03-14 21:39:05 +0700

    call API

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
+const offerRouter = require("./routers/offerRouter");
+
 //const connectDB = require('./config/db');
 require("dotenv").config();
 
@@ -13,6 +15,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(morgan("dev"));
 
+
 // define routes
+app.use('/api/offer', offerRouter);
 
 module.exports = app;
