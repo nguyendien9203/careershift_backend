e3a7a10294a52a43c8edac095d0f48a470cead20
Anhmn03 <anhmnhe176487@fpt.edu.vn>
2025-03-17 00:31:37 +0700

    update routers

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,9 +4,12 @@ const userRoutes = require("./user.route");
 const roleRoutes = require("./role.route");
 const jobRoutes = require("./job.route");
 const s3Routes = require("./s3.route");
+
 const recruitmentRoutes = require("./recruitment.route");
 const interviewRoutes = require("./interview.route");
-
+const candidateComaparisionRouter = require("../routers/candidateComparisionRouter");
+const offerRouter = require("../routers/offerRouter");
+const emailRouter = require("../routers/emailRouters");
 const router = express.Router();
 
 router.use("/auth", authRoutes);
@@ -16,5 +19,8 @@ router.use("/jobs", jobRoutes);
 router.use("/s3", s3Routes);
 router.use("/recruitments", recruitmentRoutes);
 router.use("/", interviewRoutes);
+router.use("/candidates",candidateComaparisionRouter);
+router.use("/offers",offerRouter);
+router.use("/email", emailRouter);
 
 module.exports = router;
